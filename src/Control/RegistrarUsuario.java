63e2fe5d1160feeaa6ce06cbec07c10879586112
Johan <Johan@DESKTOP-PR61TL1>
2019-10-07 21:59:25 -0500

    Entrega: Registrar Usuario

---
+++ src/Control/RegistrarUsuario.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Entidad.Usuario;
+import Entidad.Sistema;
+import Frontera.FramePrincipal;
+
+/**
+ *
+ * @author Johan
+ */
+public class RegistrarUsuario {
+    
+    private Sistema sistema = FramePrincipal.sistema;
+    
+ public String verificarDatos (String nombre,String contraseña,String confContraseña){
+        if(!verificarLongitudNombre(nombre)){
+            return("Longitud nombre incorrecta");
+        }
+        if(!verificarLongitudPassword(contraseña)){
+            return ("Longitud contraseña incorrecta");
+        }
+        if (contraseña.equals(confContraseña)){
+            Usuario usuarioNuevo = new Usuario();
+            usuarioNuevo.setNombre(nombre);
+            usuarioNuevo.setPassword(contraseña);
+            sistema.getUsuarios().add(usuarioNuevo);
+            return "Usuario Registrado con exito";
+        }
+        return "Las contraseñas ingresadas deben coincidir";
+    }
+    
+    public boolean verificarLongitudNombre (String nombre){
+        return (nombre.length() > 1 && nombre.length() <= 6);
+    }
+    
+    public boolean verificarLongitudPassword (String password){
+        return ((password.length() >= 3) && (password.length() < 6));
+    }   
+}
+
